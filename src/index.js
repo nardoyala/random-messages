d90baf241fc41e8808c71a5b7ea3714463919166
Bernardo Ayala <bernardoayala2@gmail.com>
2020-06-19 18:22:21 -0400

    paquete creado

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const messages = [
+  "Bernardo",
+  "Oscar",
+  "Ana",
+  "Gabriela",
+  "Nicolás",
+  "Carla"
+]
+
+const randomMsg = () => {
+  const message = messages[Math.floor(Math.random() * messages.length)]
+  console.log(message)
+}
+
+module.exports = { randomMsg }
